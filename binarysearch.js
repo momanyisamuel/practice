2ad50bedfceb670f196bf9b0608b0ccedb28c7e6
momanyisamuel <momanyisamuel48@gmail.com>
2021-02-03 23:47:37 +0300

    binary search practice

--- binarysearch.js
+++ binarysearch.js
@@ -24,3 +24,30 @@ const BinarySearch = (array, target) => {
 };
 
 BinarySearch(nums, 10);
+
+//recursive solution
+
+function binarySearch(array, target) {
+  // because we are not looping through our array, the values of left and right do not get
+  // automatically updated and so we need to keep track by adding them each time we call binarySearchHelper()
+  return binarySearchHelper(array, target, 0, array.length - 1);
+}
+
+const binarySearchHelper = (array, target, left, right) => {
+  if (left > right) {
+    console.log(`False Middle is ${array[mid]}`);
+    return false;
+  }
+
+  let mid = Math.floor((left = right) / 2);
+  mid;
+
+  if (target === array[mid]) {
+    console.log(`Correct Middle is ${array[mid]}`);
+    return mid;
+  } else if (target < array[mid]) {
+    return binarySearchHelper(array, target, left, mid - 1);
+  } else {
+    return binarySearchHelper(array, target, mid + 1, right);
+  }
+};
