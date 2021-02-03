5207531711ea553e34bd51e9eb7890b8b29b9cb4
momanyisamuel <momanyisamuel48@gmail.com>
2021-02-03 23:10:42 +0300

    happy coding

---
+++ binarysearch.js
@@ -0,0 +1,15 @@
+//given an array and a number n determine if n is in the array
+
+let nums = [2, 7, 8, 9, 10, 13, 17, 19, 21];
+
+const BinarySearch = (array, target) => {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left = right) / 2);
+
+    if (target) {
+    }
+  }
+};
