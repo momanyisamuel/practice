5203dc9fb5375278b2b6044e5e3347e8ed85b378
momanyisamuel <momanyisamuel48@gmail.com>
2021-02-13 23:28:50 +0300

    string sequence

--- hash.js
+++ hash.js
@@ -41,3 +41,45 @@ loop through the array forward creating each hash entry with the running total -
 loop again backwards with the running total - to include the product of all numbers after the index
 run through it one more time replacing each number in the original array with the product of all other indices to avoid creating a new array
 */
+
+const stringSequence = (s0, s1, n) => {
+  if (n == 0) return s0;
+  if (n == 1) return s1;
+  let A = [];
+  A[0] = s0;
+  A[1] = s1;
+  for (let i = 0; i < n + 1; i++) {
+    if (i % 2 === 0) {
+      A[i] = A[i - 1] + A[i - 2];
+    } else {
+      A[i] = A[i - 2] + A[i - 1];
+    }
+  }
+  return A[n];
+};
+let s0 = "a",
+  s1 = "b",
+  n = 4;
+stringSequence(s0, s1, n);
+
+// Given lowercase alphabet strings s0, s1 and a positive integer n, return the nth term of the sequence A where:
+
+// A[0] = s0
+// A[1] = s1
+// A[n] = A[n - 1] + A[n - 2] if n is even, otherwise A[n] = A[n - 2] + A[n - 1].
+// Example 1
+// Input
+// s0 = "a"
+// s1 = "b"
+// n = 4
+// Output
+// "bbaba"
+// Explanation
+// The sequence A is:
+
+// "a"
+// "b"
+// "ba" ("b" + "a")
+// "bba" ("b" + "ba")
+// "bbaba" ("bba" + "ba")
+// And A[4] = "bbaba".
